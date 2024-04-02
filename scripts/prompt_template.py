1fd6c783912a657083e6431b0c1e38e97db706df
FemkeBakker <femke.bakker02@gmail.com>
2024-04-02 14:31:37 +0000

    first experiment GEITje set-up

--- scripts/prompt_template.py
+++ scripts/prompt_template.py
@@ -1,9 +1,14 @@
 """ Includes all the prompt templates """
 
+# class list
+def get_class_list():
+    return ['Voordracht', 'Besluit', 'Schriftelijke Vragen', 'Brief', 'Raadsadres', 'Onderzoeksrapport', 'Termijnagenda', 'Raadsnotulen', 'Agenda', 'Motie', 'Actualiteit', 'Factsheets']
+
 # ------ GEITje ------------
 def simple_prompt(classes, doc):
     prompt = f"""
     Classificeer het document in één van de categoriën.
+    Houd het kort, geef enkel de naam van de categorie als response.
     
     Categoriën: {classes}
     
