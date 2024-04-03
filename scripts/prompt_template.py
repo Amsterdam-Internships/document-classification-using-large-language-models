940a010d6f64f9cc1b0926a2201a3cdfe778967f
FemkeBakker <femke.bakker02@gmail.com>
2024-04-03 11:35:05 +0000

    set up code for saving predictions

--- scripts/prompt_template.py
+++ scripts/prompt_template.py
@@ -5,12 +5,12 @@ def get_class_list():
     return ['Voordracht', 'Besluit', 'Schriftelijke Vragen', 'Brief', 'Raadsadres', 'Onderzoeksrapport', 'Termijnagenda', 'Raadsnotulen', 'Agenda', 'Motie', 'Actualiteit', 'Factsheets']
 
 # ------ GEITje ------------
-def simple_prompt(classes, doc):
+def simple_prompt(doc):
     prompt = f"""
     Classificeer het document in één van de categoriën.
     Houd het kort, geef enkel de naam van de categorie als response.
     
-    Categoriën: {classes}
+    Categoriën: {get_class_list()}
     
     Document: 
     {doc}
