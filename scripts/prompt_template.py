5173b18c2412acafa0061430944605aee16d4e8a
Ubuntu <femke.bakker02@gmail.com>
2024-04-23 10:00:39 +0000

    update experiment function to include fewshot

--- scripts/prompt_template.py
+++ scripts/prompt_template.py
@@ -5,7 +5,7 @@ def get_class_list():
     return ['Voordracht', 'Besluit', 'Schriftelijke Vragen', 'Brief', 'Raadsadres', 'Onderzoeksrapport', 'Termijnagenda', 'Raadsnotulen', 'Agenda', 'Motie', 'Actualiteit', 'Factsheets']
 
 # ------ GEITje ------------
-def simple_prompt(doc):
+def simple_prompt(doc,train_df, num_examples, text_column):
     prompt = f"""
     Classificeer het document in één van de categoriën.
     Houd het kort, geef enkel de naam van de categorie als response.
